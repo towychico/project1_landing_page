fb7cb0bea60ee88534f1dc0a40b4224a8edc2ea2
Lou <mail@towy.tech>
2022-06-25 18:30:52 -0500

    Added large Logo  color Icons and functionality

--- js/swich_flavor.js
+++ js/swich_flavor.js
@@ -1,7 +1,9 @@
-let purple_can_DOM = document.getElementById('purple_can');
-let blue_can_DOM = document.getElementById('blue_can');
-let green_can_DOM = document.getElementById('green_can');
-let red_can_DOM = document.getElementById('red_can');
+const purple_can_DOM = document.getElementById('purple_can');
+const blue_can_DOM = document.getElementById('blue_can');
+const green_can_DOM = document.getElementById('green_can');
+const red_can_DOM = document.getElementById('red_can');
+const small_logo_DOM = document.getElementsByClassName('small_logo_container')[0]; // .getElementByClassName actually returns an array
+const large_logo_DOM = document.getElementsByClassName('large_logo_container')[0];
 let cans_DOM_arr = [purple_can_DOM, blue_can_DOM, green_can_DOM, red_can_DOM];
 
 
@@ -17,8 +19,15 @@ window.onload = function() {
   purple_can_DOM.style.display = 'inherit';
 };
 
+const ChangeLogoColor = colorSTR => {
 
-function SwitchCan(can_obj){
+  small_logo_DOM.style.backgroundImage = `url(../img/nav_small_logo_${colorSTR}.svg)`;
+  large_logo_DOM.style.backgroundImage = `url(../img/nav_large_logo_${colorSTR}.svg)`;
+
+}
+function SwitchCan(can_obj, colorSTR){
+  ChangeLogoColor(colorSTR);
   HideAllCans();
   can_obj.style.display="inherit";
+
 }
